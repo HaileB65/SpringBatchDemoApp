379df9742956fa9f949ca601c9022d6bf9d7df17
HaileB65 <brendon.haile@gmail.com>
2022-08-09 18:34:18 -0500

    Uploading SpringBatchDemoApp

---
+++ src/main/java/BatchDemo/BatchDemoApp/BatchDemoAppApplication.java
@@ -0,0 +1,13 @@
+package BatchDemo.BatchDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemoAppApplication.class, args);
+	}
+
+}
